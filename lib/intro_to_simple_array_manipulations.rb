760ee8cf36f69fde33b521694d97fd6a68a977bd
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 20:08:11 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop(string)
+end
+
+def using_pop(array,string)
+
+def using_shift(array,string)
+  array.shift(string)
+end
